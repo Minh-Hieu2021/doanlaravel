1a044b5e656e16493a6734d7ad81d864d51fe1fb
VietDuc20 <96774127+VietDuc20@users.noreply.github.com>
2022-02-03 18:10:53 +0700

    Add index home
    Add index home

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user.home.index');
 });
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
