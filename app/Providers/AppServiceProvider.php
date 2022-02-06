fe456e3029314620964bb939f4a5caf1c05cb099
phamanhkhoa323 <93703554+phamanhkhoa323@users.noreply.github.com>
2022-02-06 16:06:05 +0700

    sửa lỗi view

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,11 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,10 +28,20 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Request $request)
     {
 
         Paginator::useBootStrap();
         Schema::defaultStringLength(191);
+
+        $email = Cookie::get('email');
+        $value = DB::select('select * from nhanviens where email = ?', [$email]);
+        if ($request->datedtstart == $request->datedtend) {
+            $doanhthu  = DB::select('select sum(hoadonbans.TongTien) as dt from hoadonbans where DATE(NgLap) LIKE ?', [$request->datedtstart]);
+        } else {
+            $doanhthu  = DB::select('select sum(hoadonbans.TongTien) as dt from hoadonbans where DATE(NgLap) >= ? and DATE(NgLap) <= ?', [$request->datedtstart, $request->datedtend]);
+        }
+
+        view()->share('value', $value);
     }
 }
